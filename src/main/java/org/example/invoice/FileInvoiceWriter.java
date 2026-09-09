76abb85044570b71d9b47fe5e71a5de9521b0fd0
adeeshabhanukahettiarachchi-hub <adeeshabhanukahettiarachchi@gmail.com>
2026-09-09 23:05:25 +0530

    complete FileInvoiceWriter

--- src/main/java/org/example/invoice/FileInvoiceWriter.java
+++ src/main/java/org/example/invoice/FileInvoiceWriter.java
@@ -1,4 +1,11 @@
 package org.example.invoice;
 
-public class FileInvoiceWriter {
+import java.io.FileWriter; import java.io.IOException;
+public class FileInvoiceWriter implements InvoiceWriter {
+    private final String fileName;
+    public FileInvoiceWriter(String fileName){this.fileName=fileName;}
+    public void write(String invoice){
+        try(FileWriter writer=new FileWriter(fileName)){writer.write(invoice);}
+        catch(IOException e){throw new RuntimeException("Unable to write invoice",e);}
+    }
 }
