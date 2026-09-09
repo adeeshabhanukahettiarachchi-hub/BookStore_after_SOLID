2d7fc4b118f424142b41d450a388ad3926903bc3
adeeshabhanukahettiarachchi-hub <adeeshabhanukahettiarachchi@gmail.com>
2026-09-09 23:21:33 +0530

    complete InvoiceService

--- src/main/java/org/example/service/InvoiceService.java
+++ src/main/java/org/example/service/InvoiceService.java
@@ -1,4 +1,16 @@
 package org.example.service;
 
+import org.example.invoice.InvoiceFormatter;
+import org.example.invoice.InvoiceWriter;
+import org.example.model.Purchase;
+
 public class InvoiceService {
+    private final BillingService billingService; private final InvoiceFormatter formatter; private final InvoiceWriter writer;
+    public InvoiceService(BillingService billingService, InvoiceFormatter formatter, InvoiceWriter writer){
+        this.billingService=billingService; this.formatter=formatter; this.writer=writer;
+    }
+    public String generate(Purchase purchase){
+        double total=billingService.calculateTotal(purchase);
+        String invoice=formatter.format(purchase,total); writer.write(invoice); return invoice;
+    }
 }
