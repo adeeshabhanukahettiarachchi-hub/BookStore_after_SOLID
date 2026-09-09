0874256a8e131d02e8e8eb780450f28edb662050
adeeshabhanukahettiarachchi-hub <adeeshabhanukahettiarachchi@gmail.com>
2026-09-09 23:11:49 +0530

    complete  book

--- src/main/java/org/example/model/Book.java
+++ src/main/java/org/example/model/Book.java
@@ -1,4 +1,7 @@
 package org.example.model;
 
 public class Book {
+    private final int id; private final String title; private final String author; private final double price;
+    public Book(int id,String title,String author,double price){this.id=id;this.title=title;this.author=author;this.price=price;}
+    public int getId(){return id;} public String getTitle(){return title;} public String getAuthor(){return author;} public double getPrice(){return price;}
 }
