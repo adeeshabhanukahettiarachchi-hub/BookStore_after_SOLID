d05e759b6e568d4a20e2d2859a8896be389e1d34
adeeshabhanukahettiarachchi-hub <adeeshabhanukahettiarachchi@gmail.com>
2026-09-09 23:18:21 +0530

    complete BillingService

--- src/main/java/org/example/service/BillingService.java
+++ src/main/java/org/example/service/BillingService.java
@@ -1,4 +1,11 @@
 package org.example.service;
 
+import org.example.model.Book;
+import org.example.model.Purchase;
+
 public class BillingService {
+
+    public double calculateTotal(Purchase purchase){
+        return purchase.getBooks().stream().mapToDouble(Book::getPrice).sum();
+    }
 }
