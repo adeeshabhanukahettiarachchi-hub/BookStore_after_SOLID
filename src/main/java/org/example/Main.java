13c7f62d4b3ea50c294e696cf25e025dde38cdcf
adeeshabhanukahettiarachchi-hub <adeeshabhanukahettiarachchi@gmail.com>
2026-09-09 23:23:11 +0530

    complete main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,28 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.example.invoice.FileInvoiceWriter;
+import org.example.invoice.TextInvoiceFormatter;
+import org.example.model.Book;
+import org.example.model.Customer;
+import org.example.model.Purchase;
+import org.example.repository.BookRepository;
+import org.example.repository.InMemoryBookRepository;
+import org.example.service.BillingService;
+import org.example.service.BookstoreService;
+import org.example.service.InvoiceService;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args){
+        BookRepository repository=new InMemoryBookRepository();
+        BookstoreService bookstore=new BookstoreService(repository);
+        bookstore.addBook(new Book(1,"Clean Code","Robert C. Martin",4500));
+        bookstore.addBook(new Book(2,"Effective Java","Joshua Bloch",5200));
+        bookstore.addBook(new Book(3,"Head First Java","Kathy Sierra",3900));
+        bookstore.displayBooks();
+        Customer customer=new Customer(101,"Adeesha Bhanuka","adeesha@example.com");
+        Purchase purchase=bookstore.createPurchase(1001,customer,1,3);
+        InvoiceService invoiceService=new InvoiceService(new BillingService(),
+                new TextInvoiceFormatter(),new FileInvoiceWriter("invoice.txt"));
+        System.out.println("\n"+invoiceService.generate(purchase));
     }
 }
