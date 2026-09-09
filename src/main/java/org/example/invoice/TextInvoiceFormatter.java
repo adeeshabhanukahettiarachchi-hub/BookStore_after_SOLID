c6b16cea2538566d732099a75f653b9409b64fe2
adeeshabhanukahettiarachchi-hub <adeeshabhanukahettiarachchi@gmail.com>
2026-09-09 23:10:24 +0530

    complete  TextInvoiceFormatter

--- src/main/java/org/example/invoice/TextInvoiceFormatter.java
+++ src/main/java/org/example/invoice/TextInvoiceFormatter.java
@@ -1,4 +1,13 @@
 package org.example.invoice;
 
-public class TextInvoiceFormatter {
+
+public class TextInvoiceFormatter implements InvoiceFormatter {
+    public String format(Purchase purchase,double total){
+        StringBuilder invoice=new StringBuilder("===== BOOKSTORE INVOICE =====\n");
+        invoice.append("Customer: ").append(purchase.getCustomer().getName()).append("\n");
+        for(Book book:purchase.getBooks())
+            invoice.append(book.getTitle()).append(" - Rs. ").append(String.format("%.2f",book.getPrice())).append("\n");
+        invoice.append("Total: Rs. ").append(String.format("%.2f",total)).append("\n");
+        return invoice.toString();
+    }
 }
