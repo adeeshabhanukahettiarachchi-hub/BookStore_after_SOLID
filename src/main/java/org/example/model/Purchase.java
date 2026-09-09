1cfeaf5f61aaeb6df3326f5f1ff33faa116809b6
adeeshabhanukahettiarachchi-hub <adeeshabhanukahettiarachchi@gmail.com>
2026-09-09 23:14:00 +0530

    complete Purchase

--- src/main/java/org/example/model/Purchase.java
+++ src/main/java/org/example/model/Purchase.java
@@ -1,4 +1,10 @@
 package org.example.model;
 
 public class Purchase {
+    private final int id; private final Customer customer; private final List<Book> books=new ArrayList<>();
+    public Purchase(int id,Customer customer){this.id=id;this.customer=customer;}
+    public void addBook(Book book){books.add(book);}
+    public int getId(){return id;} public Customer getCustomer(){return customer;}
+    public List<Book> getBooks(){return Collections.unmodifiableList(books);}
+
 }
